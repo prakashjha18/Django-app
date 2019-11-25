d791a8a0179c9075d154c76a181c40bcf09af242
Prakash <prkjha25@gmail.com>
2019-11-25 21:24:45 +0530

    if condition

--- travello/views.py
+++ travello/views.py
@@ -7,18 +7,21 @@ def index(request):
     dest1.desc = 'the city that never sleeps'
     dest1.img = 'destination_1.jpg'
     dest1.price = 900
+    dest1.offer = False
 
     dest2 = Destination()
     dest2.name = 'Hyderabad'
     dest2.desc = 'first biryani, then sherwani'
     dest2.img = 'destination_2.jpg'
     dest2.price = 650
+    dest2.offer = True
 
     dest3 = Destination()
     dest3.name = 'Bengaluru'
     dest3.desc = 'Beautiful city'
     dest3.img = 'destination_3.jpg'
     dest3.price = 750
+    dest3 .offer = False
 
     dests = [dest1,dest2,dest3]
     return render(request,"index.html",{'dests':dests})
