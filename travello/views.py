06ae8e64b4e641502bba92378088b1c86909a1e0
Prakash <prkjha25@gmail.com>
2019-11-25 19:35:15 +0530

    passing dynamic data in html

--- travello/views.py
+++ travello/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    dest1 = Destination()
+    dest1.name = 'Mumbai'
+    dest1.desc = 'the city that never sleeps'
+    dest1.price = 900
+    return render(request,"index.html",{'dest1':dest1})
